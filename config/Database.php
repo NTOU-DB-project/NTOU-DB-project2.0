2a039a2f0c368419393328d1d711dca23a2f2e4e
Benjamin Tsai <coding47414@gmail.com>
2024-12-17 23:37:40 +0800

    帳密私人化

--- config/Database.php
+++ config/Database.php
@@ -1,24 +1,29 @@
-<?php 
-  class Database {
-    // DB Params
-    private $host = 'localhost';
-    private $db_name = 'note_app';
-    private $username = 'root';
-    private $password = 'elena198919';
-    private $port = '3307'; 
-    private $conn;
+<?php
+class Database
+{
+  private $conn;
 
-    // DB Connect
-    public function connect() {
-      $this->conn = null;
+  // DB Connect
+  public function connect()
+  {
 
-      try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';port='.$this->port.';dbname=' . $this->db_name, $this->username, $this->password);
-        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
-      }
+    $config = include(__DIR__ . '/config.php');
 
-      return $this->conn;
+
+
+    try {
+      $this->conn = new PDO(
+        'mysql:host=' . $config['db_host'] .
+          ';port=' . $config['db_port'] .
+          ';dbname=' . $config['db_name'],
+        $config['db_username'],
+        $config['db_password']
+      );
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+      echo 'Connection Error: ' . $e->getMessage();
     }
+
+    return $this->conn;
   }
+}
